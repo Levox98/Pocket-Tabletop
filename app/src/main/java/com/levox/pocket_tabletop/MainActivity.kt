d2a5e3d8659dc20d777db5749e6b640f73277bf0
Кирилл Иванов <ivanov.ks101@gmail.com>
2023-03-25 12:45:26 +0500

    Initial commit

---
+++ app/src/main/java/com/levox/pocket_tabletop/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.levox.pocket_tabletop
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.levox.pocket_tabletop.ui.theme.PocketTabletopTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PocketTabletopTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PocketTabletopTheme {
+        Greeting("Android")
+    }
+}
