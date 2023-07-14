dfdb63a635b91f56346078da52bf6b5b31648a79
Кирилл Иванов <ivanov.ks101@gmail.com>
2023-07-14 17:19:13 +0500

    Added `core` module for utilities

---
+++ core/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    namespace = "com.pocket_tabletop.core"
+    compileSdk = Config.compileSdkVersion
+
+    defaultConfig {
+        minSdk = Config.minSdkVersion
+        targetSdk = Config.targetSdkVersion
+
+        testInstrumentationRunner = Config.testInstrumentationRunner
+        consumerProguardFiles(Config.consumerProguardFiles)
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile(Config.defaultProguardFilename),
+                Config.proguardProFilename
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = Config.jvmTarget
+    }
+}
+
+dependencies {
+
+    implementation(Dependencies.Core.coreKtx)
+}
