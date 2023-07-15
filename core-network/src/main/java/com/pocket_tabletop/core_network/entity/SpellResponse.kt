8b9db76705857bd722abc303ab14a5518b483168
Кирилл Иванов <ivanov.ks101@gmail.com>
2023-07-15 21:12:34 +0500

    Removed test entities

--- core-network/src/main/java/com/pocket_tabletop/core_network/entity/SpellResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.pocket_tabletop.core_network.entity
-
-data class SpellResponse(
-    override val count: Int?,
-    val next: String,
-    val previous: Any,
-    val results: List<Result>
-) : ApiResponse
