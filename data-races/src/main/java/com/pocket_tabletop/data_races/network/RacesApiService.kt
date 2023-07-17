1f8a2df435f1935f638a29efa5e63d73fbe1fdea
Кирилл Иванов <ivanov.ks101@gmail.com>
2023-07-17 17:44:07 +0500

    Fixed incorrect request to API

--- data-races/src/main/java/com/pocket_tabletop/data_races/network/RacesApiService.kt
+++ data-races/src/main/java/com/pocket_tabletop/data_races/network/RacesApiService.kt
@@ -2,10 +2,12 @@ package com.pocket_tabletop.data_races.network
 
 import com.pocket_tabletop.data_races.network.entity.RaceListApiEntity
 import retrofit2.Response
-import retrofit2.http.POST
+import retrofit2.http.GET
+import retrofit2.http.Headers
 
 interface RacesApiService {
 
-    @POST("/v1/races/?format=json")
+    @Headers("Content-Type: application/json")
+    @GET("/v1/races/?format=json")
     suspend fun getAllRaces(): Response<RaceListApiEntity>
 }
