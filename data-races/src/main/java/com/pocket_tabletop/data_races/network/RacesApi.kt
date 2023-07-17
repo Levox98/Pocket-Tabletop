2b700a0be5580737826ca47b5665ea892873058a
Кирилл Иванов <ivanov.ks101@gmail.com>
2023-07-17 15:32:02 +0500

    Slight API layer updates

--- data-races/src/main/java/com/pocket_tabletop/data_races/network/RacesApi.kt
+++ data-races/src/main/java/com/pocket_tabletop/data_races/network/RacesApi.kt
@@ -2,21 +2,24 @@ package com.pocket_tabletop.data_races.network
 
 import com.pocket_tabletop.core.Either
 import com.pocket_tabletop.core_network.BaseApi
-import com.pocket_tabletop.data_races.network.entity.RaceListApiEntity
+import com.pocket_tabletop.data_races.domain.entity.Race
+import com.pocket_tabletop.data_races.domain.entity.toDomain
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class RacesApi @Inject constructor(
     private val racesApiService: RacesApiService
 ) : BaseApi() {
 
-    suspend fun getRacesList(): Either<RaceListApiEntity?> =
+    suspend fun getRacesList(): Either<List<Race>?> =
         doRequest(
             tag = "getRacesList",
             request = {
                 racesApiService.getAllRaces()
             },
-            mapper = {
-                it
+            mapper = { raceList ->
+                raceList?.results?.map { it.toDomain() }
             }
         )
 }
