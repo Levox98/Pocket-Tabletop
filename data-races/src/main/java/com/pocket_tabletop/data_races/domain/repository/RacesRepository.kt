473b257e4e553fdbbd49d02903687470fb170a87
Кирилл Иванов <ivanov.ks101@gmail.com>
2023-07-17 15:34:00 +0500

    Created `races` repository

---
+++ data-races/src/main/java/com/pocket_tabletop/data_races/domain/repository/RacesRepository.kt
@@ -0,0 +1,9 @@
+package com.pocket_tabletop.data_races.domain.repository
+
+import com.pocket_tabletop.core.Either
+import com.pocket_tabletop.data_races.domain.entity.Race
+import kotlinx.coroutines.flow.Flow
+
+interface RacesRepository {
+    fun getRacesList(): Flow<Either<List<Race>>>
+}
