ef3abbb84c6e5891ae5c837ba87583fa385604b3
Кирилл Иванов <ivanov.ks101@gmail.com>
2023-07-17 15:32:02 +0500

    Slight API layer updates

--- data-races/src/main/java/com/pocket_tabletop/data_races/network/entity/RaceListApiEntity.kt
+++ data-races/src/main/java/com/pocket_tabletop/data_races/network/entity/RaceListApiEntity.kt
@@ -3,8 +3,8 @@ package com.pocket_tabletop.data_races.network.entity
 import com.pocket_tabletop.core_network.entity.ApiResponse
 
 data class RaceListApiEntity(
-    override val count: Int,
-    val next: Any,
-    val previous: Any,
+    override val count: Int?,
+    val next: String?,
+    val previous: String?,
     val results: List<RaceApiEntity>
 ) : ApiResponse
