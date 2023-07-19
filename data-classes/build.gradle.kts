fdf55ad928458449ebe885a7817ca37163dd47f5
Кирилл Иванов <ivanov.ks101@gmail.com>
2023-07-19 11:01:01 +0500

    Created `classes` data module and added necessary dependencies

---
+++ data-classes/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("com.android.library")
+    id("com.google.dagger.hilt.android")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+    namespace = "com.pocket_tabletop.data_classes"
+    compileSdk = Config.compileSdkVersion
+
+    defaultConfig {
+        minSdk = Config.minSdkVersion
+        targetSdk = Config.targetSdkVersion
+
+        testInstrumentationRunner = Config.testInstrumentationRunner
+        consumerProguardFiles(Config.consumerProguardFiles)
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile(Config.defaultProguardFilename),
+                Config.proguardProFilename
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = Config.jvmTarget
+    }
+}
+
+dependencies {
+
+    implementation(project(":core"))
+    implementation(project(":core-network"))
+
+    implementation(Dependencies.Core.coreKtx)
+
+    kapt(Dependencies.Hilt.kapt)
+    implementation(Dependencies.Hilt.hilt)
+
+    implementation(Dependencies.Retrofit.retrofit)
+    implementation(Dependencies.Retrofit.gsonConverter)
+
+    api(Dependencies.Room.api)
+    kapt(Dependencies.Room.kapt)
+    implementation(Dependencies.Room.room)
+}
